52957de8aa01d81814468e612dafa219cf8e8971
Marc Trius <derpayatz@gmail.com>
2020-11-09 13:35:49 -0600

    control form

--- src/SetupGameMain/SetupGameMain.js
+++ src/SetupGameMain/SetupGameMain.js
@@ -6,17 +6,32 @@ export default class SetupGameMain extends FormComponent {
   state = {
     error: null,
     players: {
-      value: '',
+      value: 1,
       touched: false
     },
     gameLength: {
-      value: '',
+      value: 6,
       touched: false
     },
-    hints: {
-      value: '',
+    hintLimited: {
+      value: false,
       touched: false
     },
+    hintLimit: {
+      value: 18,
+      touched: false
+    }
+  }
+
+  onhintLimitedChange = (element) => {
+    const value = element.id === 'limited'
+
+    this.setState({ hintLimited: { value, touched: true } })
+  }
+
+  onHintLimitChange = (value) => {
+    this.setState({ hintLimit: value, touched: true })
+    this.setState({ hintLimited: { value: true, touched: true }})
   }
 
   onSubmit = event => {
@@ -28,29 +43,56 @@ export default class SetupGameMain extends FormComponent {
         <h2>Set Up Game</h2>
         <form onSubmit={event => this.onSubmit(event)}>
           <p>
-            <label for='players'>Number of Players</label>
-            <input className='setup-number' id='players' type="number" value="2" />
+            <label htmlFor='players'>Number of Players: </label>
+            <input
+              onChange={e => this.onFieldChange(e.target)}
+              className='setup-number'
+              id='players' type="number"
+              value={this.state.players.value} />
           </p>
           <p>
-            <label for='gameLength'>Game Length</label>
-            <input className='setup-number' id='gameLength' type="number" value="96" />
+            <label htmlFor='gameLength'>Game Length: </label> 
+            <input
+              onChange={e => this.onFieldChange(e.target)}
+              className='setup-number'
+              id='gameLength'
+              type="number"
+              value={this.state.gameLength.value} /> stages
           </p>
           <p className='error'>Game length must be at least 18</p>
           <p>
-            <label for='hints'>Hints</label>
+            <label htmlFor='hints'>Hints</label>
           </p>
           <fieldset id='hints'>
             <p>
-              <input id='unlimited' type="radio" />
-              <label for='unlimited'>Unlimited</label>
+              <input 
+                id='unlimited' 
+                name='limitRadio'
+                type="radio"
+                onChange={e => this.onhintLimitedChange(e.target)}
+                checked={!this.state.hintLimited.value}
+                />
+              <label htmlFor='unlimited'>Unlimited</label>
             </p>
             <p>
-              <input id='limited' checked type="radio" />
-              <label for='hint-limit'>Limit to:</label>
-              <input className='setup-number' type="number" id='hint-limit' value="18" />
+              <input 
+                id='limited' 
+                name='limitRadio' 
+                type="radio" 
+                onChange={e => this.onhintLimitedChange(e.target)}
+                checked={this.state.hintLimited.value}
+                />
+              <label htmlFor='hint-limit'>Limit to:</label>
+              <input 
+                className='setup-number' 
+                type="number" 
+                id='hint-limit' 
+                value={this.state.hintLimit.value} 
+                onChange={e => this.onHintLimitChange(e.target.value)}
+                />
             </p>
           </fieldset>
-          <button className='play-button' type="submit">Start Game</button>
+          <button type="submit">Start Game</button>
         </form>
       </main>
     )
