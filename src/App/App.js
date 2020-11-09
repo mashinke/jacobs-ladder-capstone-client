667a1c0819a41b2fec9bd786bdd42fe3c7ab2d1e
Marc Trius <derpayatz@gmail.com>
2020-11-09 09:44:52 -0600

    write routes for StaticMain component

--- src/App/App.js
+++ src/App/App.js
@@ -3,112 +3,25 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from '../Header/Header';
 import Nav from '../Nav/Nav';
+import StaticMain from '../StaticMain/StaticMain';
 
 class App extends Component {
+  staticPaths() {
+    return ['/', '/rules'].map(path => {
+      return (
+        <Route exact path={path}
+          component={StaticMain}
+        />
+      )
+    })
+  }
   render() {
     console.log('App render()')
     return (
       <BrowserRouter>
         <Header />
         <Nav />
-        <main className='base game'>
-          <section className='game-time'>
-            <div>Game Time:</div>
-            <div>18:36</div>
-          </section>
-          <section className='game-status'>
-            <section className='square-container'>
-              <p>stages</p>
-              <div className='squares'>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square'></div>
-                <div className='square'></div>
-              </div>
-            </section>
-            <section className='turn-info'>
-              <ul>
-                <li>
-                  <div className='label'>Player</div>
-                  <div className='datum'>1</div>
-                </li>
-                <li>
-                  <div className='label'>Turn</div>
-                  <div className='datum'>9</div>
-                </li>
-                <li>
-                  <div className='label'>Hints</div>
-                  <div className='datum'>3/18</div>
-                </li>
-                <li>
-                  <div className='label'>Answers</div>
-                  <div className='datum'>8/10</div>
-                </li>
-                <li>
-                  <div className='label'>Challenges</div>
-                  <div className='datum'>1/2</div>
-                </li>
-              </ul>
-            </section>
-            <section className='square-container'>
-              <div className='squares'>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square filled'></div>
-                <div className='square'></div>
-                <div className='square'></div>
-                <div className='square'></div>
-                <div className='square'></div>
-              </div>
-              <p>spaces</p>
-            </section>
-          </section>
-          <form className='card'>
-            <div className='challenge-img'>challenge image</div>
-            <p className='challenge-text'>Challenge Text</p>
-            <section className='challenge-answers'>
-              <div className='column'>
-                <p>
-                  <input type='radio' />
-                  <label>Answer 1</label>
-                </p>
-                <p>
-                  <input type='radio' />
-                  <label>Answer 2</label>
-                </p>
-              </div>
-              <div className='column'>
-                <p>
-                  <input type='radio' />
-                  <label>Answer 3</label>
-                </p>
-                <p>
-                  <input type='radio' />
-                  <label>Answer 4</label>
-                </p>
-              </div>
-            </section>
-            <div className='challenge-actions'>
-              <button>Answer</button>
-              <button>Hint</button>
-            </div>
-          </form>
-          <section className='turn-actions'>
-            <button>Roll</button>
-            <button>Challenge</button>
-          </section>
-        </main>
+        {this.staticPaths()}
       </BrowserRouter>
     );
   }
