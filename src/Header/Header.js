8a76b1f4eb1dd08ae7f12730f0b0df77cbca5faf
Marc Trius <derpayatz@gmail.com>
2020-11-09 09:24:37 -0600

    fix routes

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './Header.css';
 
 export default function Header(props) {
   return (
-    <>
-      <Route exact path='/'
+    <Switch>
+      <Route path='/game'
+        render={(props) => (
+          <header className='base'>
+            <h1 className='small-masthead'>Jacob's Ladder</h1>
+          </header>
+        )} />
+      <Route path='/'
         render={(props) => (
           <header class='base'>
             <div class='hero'>
@@ -13,13 +19,7 @@ export default function Header(props) {
             </div>
           </header>
         )} />
-      <Route path='/game'
-        render={(props) => (
-          <header className='base'>
-            <h1 className='small-masthead'>Jacob's Ladder</h1>
-          </header>
-        )} />
-    </>
+    </Switch>
   )
 
 }
