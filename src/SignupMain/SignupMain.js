c6bbbb2fec3f9990d7d31d435b1717573e072943
Marc Trius <derpayatz@gmail.com>
2020-11-09 10:11:00 -0600

    control form inputs

--- src/SignupMain/SignupMain.js
+++ src/SignupMain/SignupMain.js
@@ -2,21 +2,55 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class SignupMain extends Component {
+  state = {
+    error: null,
+    email: {
+      value: '',
+      touched: false
+    },
+    pass: {
+      value: '',
+      touched: false
+    },
+    passTwo: {
+      value: '',
+      touchd: false
+    }
+  }
+
+  onFieldChange = (element) => {
+    const field = element.id;
+    const value = element.value;
+    this.setState({ [field]: { value, touched: true } })
+  }
+
   render() {
     return (
       <main className='base'>
-      <h2>Signup</h2>
-      <form>
-        <p><label htmlFor='email'>Email</label></p>
-        <p><input id='email' type="text" /></p>
-        <p><label htmlFor='pass'>Password</label></p>
-        <p><input id='pass' type="password" /></p>
-        <p><label htmlFor='pass2'>Verify Password</label></p>
-        <p><input id='pass2' type="password" /></p>
-        <button type="submit">Sign Up</button>
-      </form>
-      <p><Link to='/login'>Log In</Link></p>
-    </main>
+        <h2>Signup</h2>
+        <form>
+          <p>
+            <label htmlFor='email'>Email</label>
+          </p>
+          <p>
+            <input onChange={e => this.onFieldChange(e.target)} value={this.state.email.value} id='email' type="text" />
+          </p>
+          <p>
+            <label htmlFor='pass'>Password</label>
+          </p>
+          <p>
+            <input onChange={e => this.onFieldChange(e.target)} value={this.state.pass.value} id='pass' type="password" />
+          </p>
+          <p>
+            <label htmlFor='passTwo'>Verify Password</label>
+          </p>
+          <p>
+            <input onChange={e => this.onFieldChange(e.target)} value={this.state.passTwo.value} id='passTwo' type="password" />
+          </p>
+          <button type="submit">Sign Up</button>
+        </form>
+        <p><Link to='/login'>Log In</Link></p>
+      </main>
     )
   }
 }
