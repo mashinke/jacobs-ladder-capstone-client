2525d5f69e0cfe16ab6debd782ec971089040842
Marc Trius <derpayatz@gmail.com>
2020-11-09 08:47:11 -0600

    implement routes on header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './Header.css';
 
 export default function Header(props) {
-  console.log(props)
-  if (props.match.path === '/') {
-    return (
-      <header class='base'>
-        <div class='hero'>
-          <h1 class='masthead'>Jacob's Ladder</h1>
-        </div>
-      </header>
-    )
-  }
   return (
-    <header className='base'>
-      <h1 className='small-masthead'>Jacob's Ladder</h1>
-    </header>
+    <>
+      <Route exact path='/'
+        render={(props) => (
+          <header class='base'>
+            <div class='hero'>
+              <h1 class='masthead'>Jacob's Ladder</h1>
+            </div>
+          </header>
+        )} />
+      <Route path='/game'
+        render={(props) => (
+          <header className='base'>
+            <h1 className='small-masthead'>Jacob's Ladder</h1>
+          </header>
+        )} />
+    </>
   )
+
 }
