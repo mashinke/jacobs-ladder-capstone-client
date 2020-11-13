648b90035b5926552ae741bde14625fee856749d
Marc Trius <derpayatz@gmail.com>
2020-11-13 11:36:58 -0600

    remove lint

--- src/UserFormComponent/UserFormComponent.js
+++ src/UserFormComponent/UserFormComponent.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
 import FormComponent from '../FormComponent/FormComponent';
 import FormInput from '../FormInput/FormInput';
 
