26b109bb6803d3ed8d506ed94264afc468be1018
Marc Trius <derpayatz@gmail.com>
2020-11-23 09:22:33 -0600

    style loading component

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import FancyBorder from '../FancyBorder/FancyBorder';
+import './Loading.css';
 
 export default function Loading(props) {
   return (
-    <main>
-      <p>
-        Loading {props.label}. . .
-      </p>
+    <main className='base'>
+      <div className='modal'>
+        <FancyBorder>
+          <p className='loadingMessage'>
+          Loading {props.label}. . .
+          </p>
+      </FancyBorder>
+      </div>
     </main>
   )
 }
